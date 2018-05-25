9b4cc9b5749fb6ca46a3b1f23beb8401016f9dbc
Reksa Suhud Tri Atmojo <reksasuhud@yahoo.co.id>
2018-05-25 08:31:56 +0700

    Update Comment.java

--- app/src/main/java/lokadin/sandy/example/com/lokadin/Comment.java
+++ app/src/main/java/lokadin/sandy/example/com/lokadin/Comment.java
@@ -10,28 +10,28 @@ import android.widget.ListView;
 import android.widget.MediaController;
 import android.widget.Toast;
 import android.widget.VideoView;
-
+// activity ini untuk melakukan comment tempat dalam aplikasi
 public class Comment extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_comment);
+        setContentView(R.layout.activity_comment); //tampilkan activity comment
 
         final VideoView myVideoView = (VideoView) findViewById(R.id.myVideoView);
-        myVideoView.setVideoPath("https://youtu.be/wusW4Vzns40");
+        myVideoView.setVideoPath("https://youtu.be/wusW4Vzns40"); //play video mengenai lampung
 
         // Player control(play pause stop)
         MediaController mediaController = new MediaController(this);
-        mediaController.setAnchorView(myVideoView);
+        mediaController.setAnchorView(myVideoView); 
         myVideoView.setMediaController(mediaController);
 
-        myVideoView.start();
+        myVideoView.start(); //play video
 
-        String[] foods = {"Diskominfo", "Disdukcapil", "Dinkes", "DinasPU", "Disdik"};
+        String[] foods = {"Diskominfo", "Disdukcapil", "Dinkes", "DinasPU", "Disdik"}; //membuat list
 
         ListAdapter myAdatper = new CustomAdapter(this, foods);
-        final ListView myListView = (ListView) findViewById(R.id.myListView);
+        final ListView myListView = (ListView) findViewById(R.id.myListView); //tampilkan list dalam list view
         myListView.setAdapter(myAdatper);
 
         myListView.setOnItemClickListener(
@@ -39,7 +39,7 @@ public class Comment extends Activity {
                 @Override
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                     String food = String.valueOf(parent.getItemAtPosition(position));
-                    Toast.makeText(Comment.this, food, Toast.LENGTH_LONG).show();
+                    Toast.makeText(Comment.this, food, Toast.LENGTH_LONG).show(); //tampilkan toast nama tempat saat diklik, karena coding dapat dari github jadi variabelnya masih food :D
 
                     Intent i = new Intent(view.getContext(), DetailActivity.class);
                     i.putExtra("foodName",food);
